13c832c88d2005ae2ab99570a029f8e7e81e85b9
tzsiga <tzsiga@gmail.com>
2015-02-22 20:29:46 +0100

    looks promising

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,36 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 var Promise = require('bluebird');
-var mysql = Promise.promisifyAll(require('mysql'));
+var mysql = require('mysql');
 
 module.exports = function() {
 
-	var getAllUsers = function (res) {
-		var connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: '',
-			database: 'node_test_db'
-		});
-		 
-		connection.connect();
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'node_test_db'
+	});
 
-		connection.query('SELECT * FROM user', function (err, rows, fields) {
-			if (err) throw err;
-			res.send(rows);
+	// async model function
+	var getAllUsers = function () {
+		return new Promise(function (resolve, reject) {
+			connection.query('SELECT * FROM user', function (err, rows, fields) {
+				if (err) throw 'Database error: ' + err;
+				resolve(rows, fields);
+			});
 		});
 	};
 
-	// view lekérés
+	// view response
 	router.get('/', function (req, res) {
 		res.set('Content-Type', 'text/html');
 		res.render('user_view', { buttonLabel: 'nyomjad meg' });
 	});
 
-	// adat lekérés
-	router.get('/all', function (req, res) {
-		res.set('Content-Type', 'text/html');
-		getAllUsers(res);
+	// data response
+	router.post('/all', function (req, res) {
+		getAllUsers().then(function (rows, fields) {
+			res.set('Content-Type', 'application/json');
+			res.send(rows);
+		});
 	});
 
 	return router;
