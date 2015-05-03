9ed9945e55df7eabc46b23f828566b83d0017ba5
valyonbalazs <valyon.balazs@gmail.com>
2015-05-03 18:48:29 +0200

    calendar basics

--- index.js
+++ index.js
@@ -40,57 +40,72 @@ var connection = mysql.createConnection({
   database: 'GYM'
 });
 
-connection.connect(function(err){
-  if(!err) {
-      console.log("Database is connected ... \n\n");  
-  } else {
-      console.log("Error connecting database ... \n\n");  
-  }
-});
+
 
 app.get("/api/getAllExcercise",function(req,res){
-  var query = connection.query('SELECT DATE, WORKOUT_ID, KG, REPS, LAP FROM EXCERCISE, SESSION', function(err, rows, fields) {
+    connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... \n\n");  
+        
+        var query = connection.query('SELECT DATE, WORKOUT_ID, KG, REPS, LAP FROM EXCERCISE, SESSION', function(err, rows, fields) {
 
-  var results = [];
-  results.push(rows);
+        var results = [];
+        results.push(rows);
+          
+          query.on('end', function() {
+              connection.end();
+              return res.json(results);
+          });
+          
+          if (!err) {
+            console.log('The solution is: ', rows);
+          }
+          else
+            console.log('Error while performing Query.');
+          });
     
-    query.on('end', function() {
-        connection.end();
-        return res.json(results);
-    });
-    
-    if (!err) {
-      console.log('The solution is: ', rows);
+    } else {
+        console.log("Error connecting database ... \n\n");  
     }
-    else
-      console.log('Error while performing Query.');
-    });
+  });
+  
 });
 
 app.post("/api/addExcercise", urlencodedParser, function(req,res){
-  if (!req.body) return res.sendStatus(400);
-  else {
-    var excId = Math.floor((Math.random() * 100000) + 1);
-    
-    for(var i = 1; i < req.body.workout.length; i+=2) {
-      var workoutId = 1;
-      for(var j = 0; j < req.body.workout[i].length; j++) {
-        console.log("az i erteke: " + i + " a j erteke: " + j);
-        var kg = req.body.workout[i][j].kg;
-        var rep = req.body.workout[i][j].rep;
-        var lapCounter = j + 1;
-        var lapData = { EXCERCISE_ID: excId, WORKOUT_ID: workoutId, KG: kg, REPS: rep, LAP: lapCounter };
-        connection.query('INSERT INTO SESSION SET ?', lapData);
-      }
-      workoutId++;
+  
+  connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... \n\n");  
+        
+          if (!req.body) return res.sendStatus(400);
+          else {
+            var excId = Math.floor((Math.random() * 100000) + 1);
+            
+            for(var i = 1; i < req.body.workout.length; i+=2) {
+              var workoutId = 1;
+              for(var j = 0; j < req.body.workout[i].length; j++) {
+                console.log("az i erteke: " + i + " a j erteke: " + j);
+                var kg = req.body.workout[i][j].kg;
+                var rep = req.body.workout[i][j].rep;
+                var lapCounter = j + 1;
+                var lapData = { EXCERCISE_ID: excId, WORKOUT_ID: workoutId, KG: kg, REPS: rep, LAP: lapCounter };
+                connection.query('INSERT INTO SESSION SET ?', lapData);
+              }
+              workoutId++;
+            }
+            
+            var date =  new Date().toISOString().slice(0, 19).replace('T', ' ');
+            var userID = 1;
+            var postExcData = { DATE: date, EXCERCISE_ID: excId, USER_ID: userID };
+            connection.query('INSERT INTO EXCERCISE SET ?', postExcData);
+            connection.end();
+          }
+    } else {
+        console.log("Error connecting database ... \n\n");  
     }
-    
-    var date =  new Date().toISOString().slice(0, 19).replace('T', ' ');
-    var userID = 1;
-    var postExcData = { DATE: date, EXCERCISE_ID: excId, USER_ID: userID };
-    connection.query('INSERT INTO EXCERCISE SET ?', postExcData);
-    connection.end();
-  }
+  });
+  
+
 });
 
 
